ef0d97c25e5a99b40b0d5d5162174291a7c3a756
Karandeep-19 <karandeep01official@gmail.com>
2024-08-18 23:02:13 +0530

    User input and type casting

---
+++ src/08102024/Lab_003.py
@@ -0,0 +1,9 @@
+'''num1 = int(input("Enter 1st number: "))
+num2 =int(input("Enter 2ns number: "))
+Result =num1+num2
+print(Result)'''
+
+# Second Method
+num1 = int(input("Enter 1st number: "))
+num2 =int(input("Enter 2ns number: "))
+print(int(num1) + int(num2))
